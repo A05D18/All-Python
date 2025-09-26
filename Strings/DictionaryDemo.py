e11e482ca36c1f5ddd30749b8aa59242c55772fc
A05D18 <dbdacdacdilrajamar@gmail.com>
2025-09-26 12:24:11 +0530

    commit

---
+++ Strings/DictionaryDemo.py
@@ -0,0 +1,25 @@
+weather_data = {"pune":[23,28],
+                "mumbai":[32,37],
+                "Delhi":[35,40]}
+
+emp = {"name":"ABC",
+       "age":180,
+       "salary":200000.00,
+       "skills":["python","C++","C#"]}
+
+print("pune" in weather_data)
+print("")
+
+print("---------------")
+for key , val in weather_data.items():
+    print(key,val)
+
+for val in weather_data.values():
+    print(val)
+
+emp.update({"age":60})
+print(emp)
+
+emp["skills"] = "python"
+print(emp)
+

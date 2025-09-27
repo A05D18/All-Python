d39e3e92c7509cc8c8390ce12d0f782e22fd5fff
A05D18 <dbdacdacdilrajamar@gmail.com>
2025-09-27 13:01:39 +0530

    Day3

--- Strings/DictionaryDemo.py
+++ Strings/DictionaryDemo.py
@@ -1,28 +1,48 @@
-weather_data = {"pune":[23,28],
-                "mumbai":[32,37],
-                "Delhi":[35,40]}
+weather_data = {
+    "pune": [23, 28],
+    "mumbai": [32, 37],
+    "Delhi": [35, 40],
+    "Bangalore": [22, 29],
+    "Chennai": [30, 36],
+    "Kolkata": [29, 34],
+    "Hyderabad": [25, 32],
+    "Ahmedabad": [28, 35]
+}
 
-emp = {"name":"ABC",
-       "age":180,
-       "salary":200000.00,
-       "skills":["python","C++","C#"]}
+emp = [
+    {"name": "ABC", "age": 30, "salary": 200000.00, "skills": ["Python", "C++", "C#"]},  # Age corrected to 30
+    {"name": "Priya Sharma", "age": 29, "salary": 120000.00, "skills": ["Java", "Python", "SQL"]},
+    {"name": "Michael Chen", "age": 35, "salary": 180000.00, "skills": ["C++", "JavaScript", "Go"]},
+    {"name": "Anika Patel", "age": 26, "salary": 95000.00, "skills": ["Python", "R", "Tableau"]},
+    {"name": "Raj Kumar", "age": 42, "salary": 210000.00, "skills": ["C#", "Azure", "Python"]},
+    {"name": "Sophie Brown", "age": 31, "salary": 150000.00, "skills": ["Java", "Kotlin", "Android"]}
+]
 
 print("pune" in weather_data)
 print("")
 
 print("---------------")
-for key , val in weather_data.items():
-    print(key,val)
+# for key, val in weather_data.items():
+#     print(key, val)
+#
+# for val in weather_data.values():
+#     print(val)
+#
+# for key in weather_data.keys():
+#     print(key)
 
-for val in weather_data.values():
-    print(val)
+# emp.update["name": "ABC"]({"age": 60})
+# print(emp)
+#
+# emp["skills"] = "python"
+# print(emp)
 
-for key in weather_data.keys():
-    print(key)
+sort_by_age = list(filter(lambda item: item['age'] > 30, emp))
+print(sort_by_age)
 
-emp.update({"age":60})
-print(emp)
+sort_by_sal = sorted(emp,key=lambda item: item['salary'])
+print(sort_by_sal)
 
-emp["skills"] = "python"
-print(emp)
-
+sort_by_java = list(filter(lambda item: "Java" in item['skills']  , emp))
+print(sort_by_java)
+help(sorted)
--- functions/DemoFunctionprog.py
+++ functions/DemoFunctionprog.py
@@ -10,7 +10,7 @@ def calc_square(n):
 sq = list(map(calc_square, num))
 print(sq)
 
-sq2 = list(map(lambda n: n * n, num))
+sq2 = list(map(lambda item: item * item, num))
 print(sq2)
 
 sq3 = list(filter(lambda n: n % 2 == 0, num))
@@ -21,3 +21,6 @@ print(min_num)
 
 max_num = reduce(lambda n, n2: n if n > n2 else n2, num)
 print(max_num)
+
+filtered = list(filter(lambda item: item > 10, num))
+print(filtered)
